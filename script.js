7d1775a4af45fb1b42dbd0e06e4a93103bd1e2c9
NicolasDrp <drapier.nicolas07@gmail.com>
2023-05-19 09:43:06 +0200

    ajout : affichage stats

--- script.js
+++ script.js
@@ -35,6 +35,10 @@ document.addEventListener('DOMContentLoaded', async function () {
             let li = document.createElement('li');
             // J'affiche le numéro et le nom du Pokémon
             li.innerHTML = pokemonId + " : " + pokemonList[i].name;
+            //Si le li est cliqué , lancer la fonction fetchPokemonDetails
+            li.addEventListener('click', function () {
+                fetchPokemonDetails(pokemonId);
+            });
             // Je pousse mon li dans mon Ul qui a pour id 'pokemonList'
             document.getElementById('pokemonList').appendChild(li);
         }
@@ -50,5 +54,48 @@ document.addEventListener('DOMContentLoaded', async function () {
     function loadMorePokemon() {
         offset += limit;
         fetchPokemonList();
+    }
+
+    function fetchPokemonDetails(pokemonId) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`, 'GET', printPokemonDetails);
+    }
+
+    function printPokemonDetails() {
+        let pokemon = JSON.parse(this.responseText);
+        console.log(pokemon);
+
+        // Afficher les détails du Pokémon
+        let detailsContainer = document.getElementById('pokemonDetails');
+        detailsContainer.innerHTML = '';
+
+        //Afficher le nom
+        let name = document.createElement('p');
+        name.innerHTML = `Name: ${pokemon.name}`;
+        detailsContainer.appendChild(name);
+
+        //Afficher l'id
+        let id = document.createElement('p');
+        id.innerHTML = `ID: ${pokemon.id}`;
+        detailsContainer.appendChild(id);
+        //Afficher l'image
+        let image = document.createElement('img');
+        image.src = pokemon.sprites.front_default;
+        detailsContainer.appendChild(image);
+        //Afficher le type
+        let types = document.createElement('p');
+        types.innerHTML = `Types: ${pokemon.types.map((type) => type.type.name).join(', ')}`;
+        detailsContainer.appendChild(types);
+        //afficher le mot stats
+        let stats = document.createElement('p');
+        stats.innerHTML = 'Stats:';
+        detailsContainer.appendChild(stats);
+        //Afficher le détail des stats
+        let statsList = document.createElement('ul');
+        pokemon.stats.forEach((stat) => {
+            let statItem = document.createElement('li');
+            statItem.innerHTML = `${stat.stat.name}: ${stat.base_stat}`;
+            statsList.appendChild(statItem);
+        });
+        detailsContainer.appendChild(statsList);
     }
 });
