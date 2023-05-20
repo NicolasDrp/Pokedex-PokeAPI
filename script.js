45665a3a56b7faee2e9503296261cb4fc6ac7534
NicolasDrp <drapier.nicolas07@gmail.com>
2023-05-20 15:46:35 +0200

    doc / format : script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', async function () {
     let limit = 20;
     //div pour afficher la liste des pokemon
     let pokemonListContainer = document.getElementById('pokemonList');
+    // div de la croix pour passer containerDetails en d.none
+    let croix = document.getElementById("croix");
+    //div contenant les détails des pokemon
+    let detailsContainer = document.getElementById('pokemonDetails');
+    //div contenant detailsContainer et la croix
+    let containerDetails = document.getElementById('containerDetails')
 
-    //Afficher les 20 premier pokemon
-    fetchPokemonList();
-
-    function fetchPokemonList() {
-        fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`, 'GET', printPokemon);
-    }
 
     function fetch(url, method, fun) {
         //Initialisation de XHR
@@ -24,6 +24,68 @@ document.addEventListener('DOMContentLoaded', async function () {
         //Permet d'envoyer la requêtes
         request.send();
     }
+
+
+    //Afficher les 20 premier pokemon
+    fetchPokemonList();
+
+    //Affiche les 20 prochains pokemon
+    function fetchPokemonList() {
+        fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`, 'GET', printPokemon);
+    }
+
+    //A l'envoie du formulaire formType , lance la fonction fetchPokemonListType
+    document.getElementById('formType').addEventListener('submit', fetchPokemonListType);
+
+    //Recherche les pokemon selon leurs types
+    function fetchPokemonListType() {
+        event.preventDefault();
+        let searchInput2 = document.getElementById('searchInput2').value.toLowerCase();
+        fetch(`https://pokeapi.co/api/v2/type/${searchInput2}`, 'GET', getPokemonSearchType);
+    }
+
+    //Recupere les pokemon avec le type rechercher 
+    function getPokemonSearchType() {
+        const request = this;
+
+        if (request.status === 200) {
+            const response = JSON.parse(request.responseText);
+            const pokemonList = response.pokemon.map(pokemon => ({
+                id: pokemon.pokemon.url.split('/').filter(Boolean).pop(),
+                name: pokemon.pokemon.name
+            }));
+            displayResults(pokemonList);
+        } else {
+            console.log('Une erreur s\'est produite :', request.status);
+            displayResults([]);
+        }
+    }
+
+    //Affiche les pokemon avec le type rechercher
+    function displayResults(pokemonList) {
+
+        if (pokemonList.length === 0) {
+            pokemonListContainer.innerHTML = 'Aucun Pokémon trouvé';
+        } else {
+            pokemonListContainer.innerHTML = ''; // Supprime le contenu précédent
+
+            const ul = document.createElement('ul');
+
+            for (let i = 0; i < pokemonList.length; i++) {
+                const pokemon = pokemonList[i];
+                const li = document.createElement('li');
+                li.textContent = `${pokemon.id}: ${pokemon.name}`;
+                li.addEventListener('click', function () {
+                    fetchPokemonDetails(pokemon.id);
+                });
+                ul.appendChild(li);
+            }
+
+            pokemonListContainer.appendChild(ul);
+        }
+    }
+
+
 
     // Fonction pour filtrer la liste des pokemon en fonction de la recherche
     function filterPokemonList(pokemonList) {
@@ -41,20 +103,24 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // Fonction pour afficher les résultats de recherche des pokemon
     function printPokemonSearchResult(pokemonList) {
-        pokemonListContainer.innerHTML = '';
+        if (pokemonList.length === 0) {
+            pokemonListContainer.innerHTML = 'Aucun Pokémon trouvé';
+        } else {
+            pokemonListContainer.innerHTML = '';
 
-        for (let i = 0; i < pokemonList.length; i++) {
-            // Je récupère le numéro du pokemon à partir de l'URL
-            let pokemonUrl = pokemonList[i].url;
-            let pokemonId = pokemonUrl.split('/').filter(Boolean).pop();
-            let li = document.createElement('li');
-            li.innerHTML = `${pokemonId}: ${pokemonList[i].name}`;
-            //Si le li est cliqué , lancer la fonction fetchPokemonDetails
-            li.addEventListener('click', function () {
-                fetchPokemonDetails(pokemonId);
-            });
-            // Je pousse mon li dans mon Ul qui a pour id 'pokemonList'
-            pokemonListContainer.appendChild(li);
+            for (let i = 0; i < pokemonList.length; i++) {
+                // Je récupère le numéro du pokemon à partir de l'URL
+                let pokemonUrl = pokemonList[i].url;
+                let pokemonId = pokemonUrl.split('/').filter(Boolean).pop();
+                let li = document.createElement('li');
+                li.innerHTML = `${pokemonId}: ${pokemonList[i].name}`;
+                //Si le li est cliqué , lancer la fonction fetchPokemonDetails
+                li.addEventListener('click', function () {
+                    fetchPokemonDetails(pokemonId);
+                });
+                // Je pousse mon li dans mon Ul qui a pour id 'pokemonList'
+                pokemonListContainer.appendChild(li);
+            }
         }
     }
 
@@ -95,11 +161,16 @@ document.addEventListener('DOMContentLoaded', async function () {
         fetchPokemonList();
     }
 
+    //Récupere les données d'un pokemon à partir de son id
     function fetchPokemonDetails(pokemonId) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`, 'GET', printPokemonDetails);
     }
 
+    //Affiche les détails d'un pokemon selon son id
     function printPokemonDetails() {
+        //réaffiche la div des détails
+        containerDetails.style.display = "initial";
+        
         let pokemon = JSON.parse(this.responseText);
         console.log(pokemon);
 
@@ -140,9 +211,10 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // Fonction pour rechercher un pokemon
     function searchPokemon() {
+        event.preventDefault();
         searchQuery = document.getElementById('searchInput').value.toLowerCase();
         offset = 0; // Réinitialise l'offset pour afficher les premiers résultats de la recherche
-        fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1009`, 'GET', searchResult);
+        fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1008`, 'GET', searchResult);
     }
 
     // Fonction pour traiter les résultats de la recherche
@@ -153,6 +225,15 @@ document.addEventListener('DOMContentLoaded', async function () {
         printPokemonSearchResult(filteredPokemonList);
     }
 
-    // Bouton pour effectuer la recherche
-    document.getElementById('searchButton').addEventListener('click', searchPokemon);
+    //A l'envoie du formulaire formName , lance la fonction searchPokemon
+    document.getElementById('formName').addEventListener('submit', searchPokemon);
+
+    //Au clic de la croix , lance la fonction hidDetail
+    croix.addEventListener("click", hidDetail);
+
+    //Cache le container des détails
+    function hidDetail() {
+        containerDetails.style.display = "none"
+    }
+
 });
